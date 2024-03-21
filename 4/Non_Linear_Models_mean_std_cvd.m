9a406a5ff5f50ea2c1eb4d9ecbd59ab1cf218cb1
Arqueria <56985186+Arqueria@users.noreply.github.com>
2024-03-21 13:26:15 -0700

    Add files via upload

---
+++ 4/Non_Linear_Models_mean_std_cvd.m
@@ -0,0 +1,88 @@
+% Loading in data
+data = readtable ("OnlineNewsPopularity.csv");
+articleData = data{:,3:61};
+
+% Separating into folds
+X_stand = articleData;
+X_features = X_stand(:,1:58);
+target = X_stand(:,59);
+folds = 5;
+cv = cvpartition(size(X_features,1), 'KFold', folds);
+rmse = zeros(folds,1);
+
+
+%%
+% Set number of parameters, i.e. features
+num_param = 58;
+
+initial_run = true;
+
+for k = 1:folds
+
+    % Training and test sets for each fold
+    train_index = cv.training(k);
+    test_index = cv.test(k); 
+    X_train = X_features(train_index,:);
+    y_train = target(train_index,:);
+    X_test = X_features(test_index,:);
+    y_test = target(test_index,:);
+
+    % Setting initial guess for parameters
+    x0 = ones(1, num_param);
+    for i = 1:58
+        x0(i) = 0.001;
+    end
+
+    % Running non-linear fitting algorithm
+    parameters = lsqcurvefit(@fun, x0, X_train, y_train);
+
+    % Predicted y value
+    y_pred = fun(parameters, X_test);
+
+    % Calculate RMS error
+    rmse(k) = sqrt(mean((y_test - y_pred).^2));
+    
+    if initial_run
+        param_matrix = parameters;
+        initial_run = false;
+    else
+        param_matrix = vertcat(param_matrix, parameters);
+    end
+end
+
+mat_size = size(param_matrix,2);
+disp(mat_size);
+means = zeros(1,mat_size);
+stds = zeros(1,mat_size);
+cvds = zeros(1,mat_size);
+
+for i= 1:mat_size
+    vec = param_matrix(:,i);
+    means(i) = mean(vec);
+    stds(i) = std(vec);
+    cvds(i) = 100*stds(i) / abs(means(i));
+end
+
+%%
+
+% Non-linear prediction function for part 3d
+% Minimize (fun(x, xdata(i)) - y(i))^2
+% x is parameter vector, xdata(i) is feature vector for each data point i
+function F = fun(x, xdata)
+    F = zeros(size(xdata, 1), 1);
+    for i = 1:size(xdata, 1)
+        F(i, 1) = atan(xdata(i,:)*transpose(x));
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
